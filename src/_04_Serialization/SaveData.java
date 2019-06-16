de1093d1f1f6bdc81e618b69193b42ecfbe7f928
League Student <league@adminisorsiMac3.attlocal.net>
2019-06-16 11:31:47 -0700

    Started Pixel Art Save button! :)

--- src/_04_Serialization/SaveData.java
+++ src/_04_Serialization/SaveData.java
@@ -1,9 +1,17 @@
 package _04_Serialization;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 /*
  * Complete this class so that it can be serialized.
  */
-public class SaveData {
+public class SaveData implements Serializable{
 	public final String name;
 	public final int age;
 
@@ -11,4 +19,28 @@ public class SaveData {
 		this.name = name;
 		this.age = age;
 	}
+	
+	
+	private static void save(SaveData data) {
+		try (FileOutputStream fos = new FileOutputStream(new File("src/_04_Serialization/saved.dat")); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+			oos.writeObject(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static SaveData load() {
+		try (FileInputStream fis = new FileInputStream(new File("src/_04_Serialization/saved.dat")); ObjectInputStream ois = new ObjectInputStream(fis)) {
+			return (SaveData) ois.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException e) {
+			// This can occur if the object we read from the file is not
+			// an instance of any recognized class
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 }
