ac5cb98ac2d20ef29f3fbc40d2ecd07b8be8ed2b
League Student <league@iMac.attlocal.net>
2019-06-23 11:27:56 -0700

    Still working on Pixel Art Maker save button! :)

--- src/_04_Serialization/SaveData.java
+++ src/_04_Serialization/SaveData.java
@@ -12,6 +12,10 @@ import java.io.Serializable;
  * Complete this class so that it can be serialized.
  */
 public class SaveData implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public final String name;
 	public final int age;
 
